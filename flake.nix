e1b6285b0cae1fd4a98989579cd2a9f399eef4e3
David Siegel <dasiegel5@gmail.com>
2024-07-09 23:29:03 -0400

    working flake config

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "the drake flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    };
+
+
+    outputs = inputs@{
+        self, nixpkgs, nixpkgs-stable, ...
+    }: let
+        inherit (self) outputs;
+       in 
+    {
+    nixosConfigurations = {
+        drakenix = nixpkgs.lib.nixosSystem {
+            specialArgs = {
+                inherit inputs outputs;
+            };
+            modules = [
+                ./configuration.nix
+                ./hardware-configuration.nix
+            ];
+
+            system = "x86_64-linux";
+        };
+    };
+
+
+};
+}
